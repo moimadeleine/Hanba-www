77183799c10d8aa157fac74b8b9e20ec5e544b46
Justyna Oleś <justynaoles88@gmail.com>
2019-03-18 20:30:21 +0100

    discography slider

---
+++ js/cdswitcher.js
@@ -0,0 +1,40 @@
+
+
+
+  const discList = document.querySelector('.disc-list');
+  const li = discList.querySelectorAll('li');
+  let currentSlide = 'slider1';
+  const songs = document.querySelector('.songs-list');
+  const song = songs.querySelectorAll('.song');
+
+  
+  
+  discList.addEventListener('click', function(e){
+  
+  
+    if (e.target.tagName=="LI") {
+  
+      let activeSlide = e.target.className;
+  
+  
+    
+  
+      for (let i =0; i<song.length; i++) {
+  
+        song[i].classList.remove('active');
+  
+      }
+  
+      songs.querySelector('.'+activeSlide).classList.add('active');
+  
+  
+    }
+  
+
+  });
+  
+
+
+
+
+
