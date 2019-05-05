193ecec71713fd8e80002bd22a9f3d6777f6e7ab
Justyna Oleś <justynaoles88@gmail.com>
2019-05-05 18:00:43 +0200

    scroll option mobile device

--- js/cdswitcher.js
+++ js/cdswitcher.js
@@ -5,9 +5,11 @@
   const discList = document.querySelector('.disc-list');
   const songs = document.querySelector('.songs-list');
   const song = songs.querySelectorAll('.song');
+  let windowWidth = window.innerWidth;
+  let mobileDevice =windowWidth < 768;
 
 
-
+  addScrollTo();
 
   if (document.querySelector('.close-cd')!=null)  {
 
@@ -17,7 +19,6 @@
 
     closeCd.addEventListener('click', function(){
     
-    
         cdContent.remove();
     
       });
@@ -26,12 +27,12 @@
   
   
   discList.addEventListener('click', function(e){
+
   
-  
-    if (e.target.tagName=="LI") {
+    if (e.target.tagName=="LI" || e.target.tagName=="A") {
+
   
       let activeSlide = e.target.className;
-  
   
     
   
@@ -43,10 +44,38 @@
   
       songs.querySelector('.'+activeSlide).classList.add('active');
   
-  
     }
   
   });
+
+  //scroll option from song title to text for mobile device only
+  function addScrollTo(){
+
+    if(mobileDevice)
+
+    $(songs).attr('id','texts');
+
+  }
+
+
+  $(window).resize(function(){
+   
+    let windowWidth = window.innerWidth;
+   
+
+    if (windowWidth < 768) {
+
+      $(songs).attr('id','texts');
+    }
+
+    else {
+
+      $(songs).removeAttr('id','texts');
+    }
+
+  });
+
+
 
 })();
   
